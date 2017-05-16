6e6fce103af43d119695d32a27ec33a2cc3b55c2
Ignacio Norambuena <inorambuenaa@gmail.com>
2017-05-16 00:36:04 -0400

    first commit

---
+++ app/src/main/java/com/byobdev/kamal/initiative.java
@@ -0,0 +1,13 @@
+package com.byobdev.kamal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class initiative extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initiative);
+    }
+}
