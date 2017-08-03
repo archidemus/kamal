11bb5642bed2fe82344d600e5c0b14de7f192b17
Ignacio Norambuena <inorambuenaa@gmail.com>
2017-08-03 17:29:38 -0400

    Reparado error login. Falta autenticación de google

--- app/src/main/java/com/byobdev/kamal/LoginActivity.java
+++ app/src/main/java/com/byobdev/kamal/LoginActivity.java
@@ -139,7 +139,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Auth.GoogleSignInApi.signOut(mGoogleApiClient);
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
