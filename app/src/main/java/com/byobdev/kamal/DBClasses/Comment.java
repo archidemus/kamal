ecfdcb8cefb6abf1ad1e570cb2901cd27bcacb85
Kurono3173 <cristobal.scheihi.12@sansano.usm.cl>
2017-10-08 03:13:49 -0300

    Comentarios funcionando

---
+++ app/src/main/java/com/byobdev/kamal/DBClasses/Comment.java
@@ -0,0 +1,22 @@
+package com.byobdev.kamal.DBClasses;
+
+/**
+ * Created by crono on 06-10-17.
+ */
+
+public class Comment {
+
+    public String Nombre;
+    public String Image;
+    public String Comentario;
+
+
+    public Comment(String Nombre, String Image, String Comentario){
+        this.Nombre = Nombre;
+        this.Image = Image;
+        this.Comentario = Comentario;
+    }
+
+    public Comment(){ }
+
+}
