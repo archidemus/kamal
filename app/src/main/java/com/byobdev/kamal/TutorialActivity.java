406d3c150b71066f6736bbdce71d63344cf83d6e
Ignacio Norambuena <inorambuenaa@gmail.com>
2017-10-25 16:57:09 -0300

    Se me actualizo android studio y cambio cosas

--- app/src/main/java/com/byobdev/kamal/TutorialActivity.java
+++ app/src/main/java/com/byobdev/kamal/TutorialActivity.java
@@ -30,10 +30,10 @@ public class TutorialActivity extends AppIntro2 {
 
         // Instead of fragments, you can also use our default slide
         // Just set a title, description, background and image. AppIntro will do the rest.
-        addSlide(AppIntroFragment.newInstance("Encuentra", "Entérate de lo que ocurre a tu alrededor. Las marcas en el mapa te dirán la categoría", R.mipmap.ic_launcher, getResources().getColor(R.color.Primary)));
-        addSlide(AppIntroFragment.newInstance("Marcas", "Los colores indican el estado de la iniciativa", R.mipmap.ic_launcher, getResources().getColor(R.color.Primary)));
-        addSlide(AppIntroFragment.newInstance("Filtra por palabra", "Escribe lo que necesitas y te diremos si alguien cerca lo tiene", R.mipmap.ic_launcher, getResources().getColor(R.color.Primary)));
-        addSlide(AppIntroFragment.newInstance("¿Tienes tiempo libre?", "Explora lo que ocurrirá en el día con nuestra linea de tiempo", R.mipmap.ic_launcher, getResources().getColor(R.color.Primary)));
+        addSlide(AppIntroFragment.newInstance("Encuentra", "Entérate de lo que ocurre a tu alrededor. Las marcas en el mapa te dirán la categoría", R.drawable.ic_helpimages1, getResources().getColor(R.color.Primary)));
+        addSlide(AppIntroFragment.newInstance("Marcas", "Los colores indican el estado de la iniciativa", R.drawable.ic_helpimages2, getResources().getColor(R.color.Primary)));
+        addSlide(AppIntroFragment.newInstance("Filtra", "Por categoría o palabra clave usando los íconos en la barra superior", R.drawable.ic_helpimages3, getResources().getColor(R.color.Primary)));
+        addSlide(AppIntroFragment.newInstance("¿Tienes tiempo libre?", "Ve que ocurrirá a esa hora usando la línea de tiempo. Elige un rango de tiempo y muevelo a lo largo del día.", R.mipmap.ic_launcher, getResources().getColor(R.color.Primary)));
         // OPTIONAL METHODS
         // Override bar/separator color.
         setBarColor(getResources().getColor(R.color.Primary));
